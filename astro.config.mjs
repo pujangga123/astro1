f89f452d56c79d12aa66622c2bd0ff34a08d34d2
En Tay <rush_x@yahoo.com>
2024-10-03 23:09:01 +0700

    finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://neon-pavlova-f6c1ac.netlify.app/"
+  site: "https://neon-pavlova-f6c1ac.netlify.app/",
+  integrations: [preact()]
 });
